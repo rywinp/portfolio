92512162047d933c22cb89c67b7a2c3bfdb1d7cc
Rywin Patcharaput <rywinp@umich.edu>
2024-12-13 15:51:27 -0500

    Added opacity to header when scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 'use client'
 
+import { headers } from "next/headers";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [headerStatus, setHeaderStatus] = useState(true)
+  const [headerOpacity, setHeaderOpacity] = useState(1)
   
   useEffect(() => {
 
     const handleScroll = () => {
-      if(window.scrollY >= 50) {
-        setHeaderStatus(false)
+      
+      if(window.scrollY >= 1 && window.scrollY <= 200) {
+        setHeaderOpacity(1/ (window.scrollY / 150 + 1));
+      }
+      else if(window.scrollY == 0) {
+        setHeaderOpacity(1);
       }
       else {
-        setHeaderStatus(true)
+        setHeaderOpacity(0);
       }
     };
 
@@ -26,9 +32,23 @@ export default function Home() {
 
   return(
     <>
-      <header>
-        <Image className={`w-1/2 mx-auto ${headerStatus ? `visible` : `invisible`}`} src="/RywinDennisRectangle.jpeg" alt="Rywin by the campfire" width={500} height={150} priority/>
-      </header>
+      <div className="relative w-full flex justify-center items-center h-screen">
+        {/* Background image with opacity */}
+        <div
+          className={`absolute inset-0 bg-cover bg-center transition duration-150`}
+          style={{
+            backgroundImage: `url(/RywinDennisRectangle.jpeg)`,
+            opacity: `${headerOpacity}`,
+          }}
+        ></div>
+
+        {/* Content */}
+        <h1 className="absolute left-0 bottom-0 text-9xl font-bold text-white leading-tight">
+          Rywin Patcharaput
+        </h1>
+      </div>
+
+
       <div className="h-[1000px]">
         end page
       </div>
@@ -36,4 +56,5 @@ export default function Home() {
   );
 }
 
+//         <Image className={`transition-opacity duration-250 ${headerStatus ? `visible opacity-100` : `opacity-0`}`} src="/RywinDennisRectangle.jpeg" alt="Rywin by the campfire" width={1100} height={0} priority/>
 // <Image src="/RywinDennisSquare.png" alt="Rywin by the campfire" width={150} height={150} priority />
