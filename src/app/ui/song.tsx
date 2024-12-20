7bc492ea278c9a82f3ea8dac9a20e75f5f6a9ee8
Rywin Patcharaput <rywinp@umich.edu>
2024-12-20 06:48:39 -0500

    whitespace-nowrap will not make lines go into \n automagically

--- src/app/ui/song.tsx
+++ src/app/ui/song.tsx
@@ -6,7 +6,7 @@ export default function Song({song}) {
         <div className="flex items-center gap-5 relative">
             <span className="font-semibold">{song.id}</span>
             <img src={song.path} className="w-[70px] h-[70px]"></img>
-            <span className="font-semibold">{song.title} :</span>
+            <span className="font-semibold whitespace-nowrap">{song.title} :</span>
             <span className="font-semibold">{song.description}</span>
             <div className="w-1/12"/>
             <img src={playPurplePath} className="absolute right-0 w-[50px] h-[50px]"></img>
