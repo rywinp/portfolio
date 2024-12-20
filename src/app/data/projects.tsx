dc8e7f9289b51d274b2bb834f7937ca14cf4b6bd
rywinp <rywinp@umich.edu>
2024-12-20 06:41:21 -0500

    Add more desc to road report

--- src/app/data/projects.tsx
+++ src/app/data/projects.tsx
@@ -1,4 +1,4 @@
 export const ProjectList = [
     { id: 1, title: 'Playbooks', path: '/projects/newPlaybooks.png', description: "Analyze and convert basketball footage into interactive clipboard", tech: "YOLOv8, Roboflow, HTML, Tailwind CSS, Django, JavaScript"},
-    { id: 2, title: "Road Report", path: "/projects/RoadReport.webp", description: "GPS-based pothole reporting app alerting users and city officials", tech: "Next.js, Flask, Google Maps API / Geocoding API, Tailwind CSS,"}
+    { id: 2, title: "Road Report", path: "/projects/RoadReport.webp", description: "GPS-Based pothole reporting app alerting users that generates automated reports to notify city officials", tech: "Next.js, Flask, Google Maps API / Geocoding API, Tailwind CSS,"}
 ]
